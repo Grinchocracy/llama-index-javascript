a42c121994f690a7058332e082b8f932735dc805
Bradley Cleary <selfaril@live.com.au>
2024-11-26 14:37:50 +0000

    Initial change

--- app/api/chat/engine/loader.ts
+++ app/api/chat/engine/loader.ts
@@ -1,9 +1,32 @@
-import { SimpleDirectoryReader } from "llamaindex";
+import { FILE_EXT_TO_READER, SimpleDirectoryReader } from "llamaindex";
+import { PDFReader } from 'llamaindex/readers/PDFReader';
+// import { PyMuPDFReader } from 'llamaindex/readers/PyMuPDFReader';
+// import { LlamaParseReader } from "llamaindex/readers/LlamaParseReader";
 
 export const DATA_DIR = "./data";
 
+// export async function getDocuments() {
+//   return await new SimpleDirectoryReader().loadData({
+//     directoryPath: DATA_DIR,
+//   });
+
 export async function getDocuments() {
+  var parser = new PDFReader();
+  var file_extractor = {".pdf": parser};
   return await new SimpleDirectoryReader().loadData({
     directoryPath: DATA_DIR,
+    fileExtToReader: file_extractor
   });
+
+// export async function getDocuments() {
+//   const reader = new SimpleDirectoryReader();
+//   // Load PDFs using LlamaParseReader
+//   return await reader.loadData({
+//     directoryPath: DATA_DIR,
+//     fileExtToReader: {
+//       pdf: new LlamaParseReader({ resultType: "markdown" }),
+//     },
+//   });
+
+
 }
